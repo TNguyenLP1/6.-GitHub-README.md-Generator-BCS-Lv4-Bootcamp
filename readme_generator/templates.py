5631cabeebb31eb706ecd2e8f2b17c89bbbac532
Tri Nguyen <nhatminhtri.nun@icloud.com>
2025-10-23 22:51:50 +0100

    v3.0.0 - adding main function and progress bar

---
+++ readme_generator/templates.py
@@ -0,0 +1,29 @@
+README_TEMPLATE = """# {title}
+
+## Description
+{description}
+
+---
+
+## Installation
+{installation}
+
+---
+
+## Usage
+{usage}
+
+---
+
+## License
+This project is licensed under the **{license}**.
+
+---
+
+## Author
+**{author}**
+
+Contact: {contact}
+"""
+
+# Setting up template, to ensure consistency across all README.md for all projects on Github Repo
